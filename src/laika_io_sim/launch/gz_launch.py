bfc1dd365bffb1762b3f6cd861a3837568e73a83
Robert Mones <robertmones42@gmail.com>
2025-04-19 11:42:05 -0400

    Updated gz_launch to add torque sensors

--- src/laika_io_sim/launch/gz_launch.py
+++ src/laika_io_sim/launch/gz_launch.py
@@ -6,18 +6,31 @@ def generate_launch_description():
         package="ros_gz_bridge",
         executable="parameter_bridge",
         arguments=[
-            "/fl_knee_torque@std_msgs/msg/Float64]ignition.msgs.Double",
-            "/fl_hip_torque@std_msgs/msg/Float64]ignition.msgs.Double",
-            "/fl_lat_torque@std_msgs/msg/Float64]ignition.msgs.Double",
-            "/fr_knee_torque@std_msgs/msg/Float64]ignition.msgs.Double",
-            "/fr_hip_torque@std_msgs/msg/Float64]ignition.msgs.Double",
-            "/fr_lat_torque@std_msgs/msg/Float64]ignition.msgs.Double",
-            "/rr_knee_torque@std_msgs/msg/Float64]ignition.msgs.Double",
-            "/rr_hip_torque@std_msgs/msg/Float64]ignition.msgs.Double",
-            "/rr_lat_torque@std_msgs/msg/Float64]ignition.msgs.Double",
-            "/rl_knee_torque@std_msgs/msg/Float64]ignition.msgs.Double",
-            "/rl_hip_torque@std_msgs/msg/Float64]ignition.msgs.Double",
-            "/rl_lat_torque@std_msgs/msg/Float64]ignition.msgs.Double"
+            "/fl_knee_torque_cmd@std_msgs/msg/Float64]ignition.msgs.Double",
+            "/fl_hip_torque_cmd@std_msgs/msg/Float64]ignition.msgs.Double",
+            "/fl_lat_torque_cmd@std_msgs/msg/Float64]ignition.msgs.Double",
+            "/fr_knee_torque_cmd@std_msgs/msg/Float64]ignition.msgs.Double",
+            "/fr_hip_torque_cmd@std_msgs/msg/Float64]ignition.msgs.Double",
+            "/fr_lat_torque_cmd@std_msgs/msg/Float64]ignition.msgs.Double",
+            "/rr_knee_torque_cmd@std_msgs/msg/Float64]ignition.msgs.Double",
+            "/rr_hip_torque_cmd@std_msgs/msg/Float64]ignition.msgs.Double",
+            "/rr_lat_torque_cmd@std_msgs/msg/Float64]ignition.msgs.Double",
+            "/rl_knee_torque_cmd@std_msgs/msg/Float64]ignition.msgs.Double",
+            "/rl_hip_torque_cmd@std_msgs/msg/Float64]ignition.msgs.Double",
+            "/rl_lat_torque_cmd@std_msgs/msg/Float64]ignition.msgs.Double"
+        ] + [
+            "/forcetorque/fl_knee@geometry_msgs/msg/WrenchStamped[ignition.msgs.Wrench",
+            "/forcetorque/fl_hip@geometry_msgs/msg/WrenchStamped[ignition.msgs.Wrench",
+            "/forcetorque/fl_lat@geometry_msgs/msg/WrenchStamped[ignition.msgs.Wrench",
+            "/forcetorque/fr_knee@geometry_msgs/msg/WrenchStamped[ignition.msgs.Wrench",
+            "/forcetorque/fr_hip@geometry_msgs/msg/WrenchStamped[ignition.msgs.Wrench",
+            "/forcetorque/fr_lat@geometry_msgs/msg/WrenchStamped[ignition.msgs.Wrench",
+            "/forcetorque/rr_knee@geometry_msgs/msg/WrenchStamped[ignition.msgs.Wrench",
+            "/forcetorque/rr_hip@geometry_msgs/msg/WrenchStamped[ignition.msgs.Wrench",
+            "/forcetorque/rr_lat@geometry_msgs/msg/WrenchStamped[ignition.msgs.Wrench",
+            "/forcetorque/rl_knee@geometry_msgs/msg/WrenchStamped[ignition.msgs.Wrench",
+            "/forcetorque/rl_hip@geometry_msgs/msg/WrenchStamped[ignition.msgs.Wrench",
+            "/forcetorque/rl_lat@geometry_msgs/msg/WrenchStamped[ignition.msgs.Wrench"
         ]
     )
 
