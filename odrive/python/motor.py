a68b79abd37ddd7e500080d613932730e06eb35c
alexmnr <alexander@minor.pw>
2025-11-14 19:35:42 -0500

    start working on torque controll

--- odrive/python/motor.py
+++ odrive/python/motor.py
@@ -11,23 +11,72 @@ class Motor:
         self.bus = bus
         print(f"Motor {self.id} \"{self.name}\" initialized")
 
-    def set_mode(self, mode):
-        if mode == "CLOSED_LOOP_CONTROL":
-            state = 0x08 # 8: AxisState.CLOSED_LOOP_CONTROL
+    def set_mode(self, control_mode, input_mode):
+        if control_mode == "VOLTAGE_CONTROL":
+            control_code = 0x00
+        elif control_mode == "TORQUE_CONTROL":
+            control_code = 0x01
+        elif control_mode == "VELOCITY_CONTROL":
+            control_code = 0x02
+        elif control_mode == "POSITION_CONTROL":
+            control_code = 0x03
+        else:
+            print("Error: Control Mode not recognized!")
+            exit(1)
+
+        if input_mode == "INACTIVE":
+            input_code = 0x00
+        elif input_mode == "PASSTHROUGH":
+            input_code = 0x01
+        elif input_mode == "VEL_RAMP":
+            input_code = 0x02
+        elif input_mode == "POS_FILTER":
+            input_code = 0x03
+        elif input_mode == "TRAP_TRAJ":
+            input_code = 0x05
+        elif input_mode == "TORQUE_RAMP":
+            input_code = 0x06
+        elif input_mode == "TUNING":
+            input_code = 0x08
+        else:
+            print("Error: Input Mode not recognized!")
+            exit(1)
+
+        self.bus.send(can.Message(
+            arbitration_id=(self.id << 5 | 0x0b), # 0x0b: Set_Controller_Mode
+            data=struct.pack('<II', control_code, input_code), 
+            is_extended_id=False
+        ))
+
+        for msg in bus:
+            if msg.arbitration_id == (self.id << 5 | 0x01): # 0x01: Heartbeat
+                _, res, _, _ = struct.unpack('<IBBB', bytes(msg.data[:7]))
+                print(res)
+                # if res == code: 
+                he, hu = struct.unpack('<ii', bytes(msg.data))
+                print(f"Motor {self.id} \"{self.name}\" set to mode {he} {hu}")
+                break
+
+
+    def set_state(self, state):
+        if state == "CLOSED_LOOP_CONTROL":
+            code = 0x08 # 8: AxisState.CLOSED_LOOP_CONTROL
+        elif state == "MOTOR_CALIBRATION":
+            code = 0x04
         else:
             print("Error: State not recognized!")
             exit(1)
         self.bus.send(can.Message(
             arbitration_id=(self.id << 5 | 0x07), # 0x07: Set_Axis_State
+            data=struct.pack('<I', code), # 8: AxisState.CLOSED_LOOP_CONTROL
             is_extended_id=False
         ))
 
-        # Wait for axis to enter closed loop control by scanning heartbeat messages
         for msg in bus:
             if msg.arbitration_id == (self.id << 5 | 0x01): # 0x01: Heartbeat
-                _, state, _, _ = struct.unpack('<IBBB', bytes(msg.data[:7]))
-                if state == state: # 8: AxisState.CLOSED_LOOP_CONTROL
-                    print(f"Motor {self.id} \"{self.name}\" set to mode {mode}")
+                _, res, _, _ = struct.unpack('<IBBB', bytes(msg.data[:7]))
+                if res == code: 
+                    print(f"Motor {self.id} \"{self.name}\" set to mode {state}")
                     break
 
     def set_speed(self, speed, torque=0.0):
@@ -78,16 +127,17 @@ if __name__ == "__main__":
 
     # Examples
     for motor in motors:
-        motor.set_mode("CLOSED_LOOP_CONTROL")
+        # motor.set_state("CLOSED_LOOP_CONTROL")
+        motor.set_mode("TORQUE_CONTROL", "PASSTHROUGH")
 
-    for speed in range(1,20,3):
-        for motor in motors:
-            motor.set_speed(speed)
-        time.sleep(1)
+    # for speed in range(1,20,3):
+    #     for motor in motors:
+    #         motor.set_speed(speed)
+    #     time.sleep(1)
 
-    for i in range(100):
-        for motor in motors:
-            pos, vel = motor.get_position()
+    # for i in range(100):
+    #     for motor in motors:
+    #         pos, vel = motor.get_position()
 
     for motor in motors:
         motor.stop()
